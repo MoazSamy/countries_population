c65462f6b0ec67d570682f112bdea3e26d4363e7
Moaz Samy <moazsamy333@gmail.com>
2022-12-22 11:31:03 +0200

    Cleaning up.

--- population/models.py
+++ population/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 
-# Create your models here.
 class Population(models.Model):
     country = models.CharField(max_length=100)
     year = models.PositiveSmallIntegerField()
