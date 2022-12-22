005e162067aed35c58fb8801f334cf06d21a0094
Moaz Samy <moazsamy333@gmail.com>
2022-12-22 13:13:44 +0200

    Refactoring populate, Adding country detail api.

--- population/views.py
+++ population/views.py
@@ -1,26 +1,12 @@
 from .models import Population
 from .serializers import PopulationSerializer
 from rest_framework import generics
-import requests
+
 class PopulationList(generics.ListAPIView):
     queryset = Population.objects.all()
     serializer_class = PopulationSerializer
 
-def PopulationRetrieve(request):
-    all_pops = {}
-    url = "https://countriesnow.space/api/v0.1/countries/population"
-    response = requests.get(url)
-    data = response.json()
-    countries = data['data']
-    for i in countries:
-        population_counts = i['populationCounts']
-        for j in population_counts:
-            country_attrs = {
-                "country" : i['country'],
-                "year" : j['year'],
-                "pop" : j['value'],
-            }
-            Population.objects.create(**country_attrs)
-    all_pops = Population.objects.all().order_by('country')
-    
-    return all_pops
+class CountryDetailAPI(generics.ListAPIView):
+    serializer_class = PopulationSerializer
+    def get_queryset(self):
+        return Population.objects.filter(code=self.kwargs['code'])
