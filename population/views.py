5d0589d81143b7ab7bba6f1d5c9294083a316331
Moaz Samy <moazsamy333@gmail.com>
2022-12-22 15:33:35 +0200

    Added case where the user enters unidentifed code.

--- population/views.py
+++ population/views.py
@@ -1,12 +1,20 @@
 from .models import Population
 from .serializers import PopulationSerializer
 from rest_framework import generics
+from django.http import Http404
+
 
 class PopulationList(generics.ListAPIView):
     queryset = Population.objects.all()
     serializer_class = PopulationSerializer
 
+
 class CountryDetailAPI(generics.ListAPIView):
     serializer_class = PopulationSerializer
+
     def get_queryset(self):
-        return Population.objects.filter(code=self.kwargs['code'])
+        populations = Population.objects.filter(code=self.kwargs["code"])
+        if not populations:
+            raise Http404
+        else:
+            return populations
