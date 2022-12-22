6e8dbdc3fb375f77cab6b919928ea7cbffe440c8
Moaz Samy <moazsamy333@gmail.com>
2022-12-22 13:10:51 +0200

    Adding Code field to help identify records.

--- population/models.py
+++ population/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 class Population(models.Model):
     country = models.CharField(max_length=100)
+    code = models.CharField(max_length=10)
     year = models.PositiveSmallIntegerField()
     pop = models.PositiveBigIntegerField()
 
